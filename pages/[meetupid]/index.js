b6933bda7657e002b4cf85ba35bc5814d38a16a5
recev <mryildiz702@gmail.com>
2022-02-10 12:11:18 +0300

    removing embbeded ids

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -66,13 +66,14 @@ export async function getStaticPaths() {
         const url = "mongodb+srv://reciko:tekeve72@firstcluster.un4gx.mongodb.net/meetups?retryWrites=true&w=majority";
         const client = new MongoClient(url);
 
+        let  meetupPaths = []
         try {
             await client.connect();
             const meetupsDb = client.db('meetups');
             const meetupCollection = meetupsDb.collection('meetups');
             const meetupData = await meetupCollection.find({}, { _id: 1 }).toArray();
 
-            const meetupPaths = meetupData.map(item => {
+            meetupPaths = meetupData.map(item => {
                 const meetupId = ObjectId(item._id).toString();
                 return {
                     params: {
@@ -81,19 +82,28 @@ export async function getStaticPaths() {
                 };
             });
 
-            return meetupPaths;
-
         } finally {
             await client.close();
         }
+
+        return meetupPaths;
     };
 
     const meetupPaths = await getMeetupPaths().catch(error => console.log(error));
     console.log('rmeetups', meetupPaths);
 
     return {
-        paths: meetupPaths,
-        fallback: true
+        paths: [
+  { params: { meetupid: '62026b39b9af02338f94bbeb' } },
+  { params: { meetupid: '62026beaa164abeeec866a1b' } },
+  { params: { meetupid: '62026c51a164abeeec866a1c' } },
+  { params: { meetupid: '62026c54a164abeeec866a1d' } },
+  { params: { meetupid: '62026ccfa164abeeec866a1e' } },
+  { params: { meetupid: '620272cea164abeeec866a1f' } },
+  { params: { meetupid: '6204bc6ced98eb5d43830f6c' } },
+  { params: { meetupid: '6204c1c8ed98eb5d43830f6d' } }
+],
+        fallback: 'blocking'
     }
 }
 
